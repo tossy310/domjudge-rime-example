491f40690dedcf3bd11d7f4e8960c7e38c2fa050
Hiroki Sato <hsugar.310@gmail.com>
2024-08-26 23:03:19 +0900

    Add boolfind taken from DOMjudge repo

---
+++ boolfind/c-wa/main.c
@@ -0,0 +1,11 @@
+/*
+ * Sample solution in C for the "boolfind" interactive problem.
+ * Exits immediately without any output.
+ *
+ * @EXPECTED_RESULTS@: WRONG-ANSWER
+ */
+
+int main()
+{
+	return 0;
+}
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for i in {1..99999}; do
+  echo -n "1 " 
+done
+
+echo "0" 
