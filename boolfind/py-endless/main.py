4d6b8d2ab400e886714b144d2e4d05e1c6761b99
Hiroki Sato <hsugar.310@gmail.com>
2024-08-30 23:31:08 +0900

    Add two wrong py solution

---
+++ boolfind/py-endless/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''
+Sample solution in Python for the "boolfind" interactive problem.
+
+@EXPECTED_RESULTS@: WRONG-ANSWER
+'''
+
+for i in range(9999999999):
+	print('abcd' * 1000)
